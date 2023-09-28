a06bcb1da7b4296cde9c4f8f14fea0028aa99ce3
RyanJohnKennedy <Ryan.Kennedy.John@gmail.com>
2023-09-28 17:56:22 +0200

    Initial commit

---
+++ Assets/UI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    private void Awake()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventManager.Instance.AddListener(EVENT_TYPE.GAME_INIT, OnWelcomeMessage);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void OnWelcomeMessage(EVENT_TYPE Event_Type, Component Sender, object Param = null)
+    {
+        Debug.Log("Welcome to my Game");
+    }
+}
